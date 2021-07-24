f9763cecd62e800aac3838f5047dad57c8a51fe5
Jimmy Zhang <jimmy.zhang@microsoft.com>
2021-07-24 14:53:47 -0700

    basic working app with help button

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,43 @@
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
 
-const fetch = require('node-fetch');
-const DEBUG = false;
+const HELP_BUTTON_POSITION = {x: 0.5, y: 0.5, z: 0}
 
-export default class JimmyBot {
-	public assets: MRE.AssetContainer;
+export default class App {
+  public assets: MRE.AssetContainer;
 
-	constructor(public context: MRE.Context, public params: MRE.ParameterSet) {
-	  this.assets = new MRE.AssetContainer(context);
-
+  constructor(public context: MRE.Context, public params: MRE.ParameterSet) {
+    this.assets = new MRE.AssetContainer(context);
     this.context.onStarted(() => this.started());
-    this.context.onUserLeft(user => this.userLeft(user));
-    this.context.onUserJoined(user => this.userJoined(user));
-	}
-
-	private async started() {
-	}
-
-  private userLeft(user: MRE.User) {
   }
 
-  private userJoined(user: MRE.User) {
+  private async started() {
+    this.createHelpButton();
   }
+
+  private createHelpButton() {
+    let text = `Welcome to the Directory!\n\nhelp\n\nLearn more at github.com/tuesy/jimmybot`;
+    const button = MRE.Actor.CreateFromLibrary(this.context, {
+      resourceId: 'artifact:1579238405710021245',
+      actor: {
+        name: 'Help Button',
+        transform: { local: { position: HELP_BUTTON_POSITION } },
+        collider: { geometry: { shape: MRE.ColliderType.Box, size: { x: 0.5, y: 0.2, z: 0.01 } } }
+      }
+    });
+    button.setBehavior(MRE.ButtonBehavior).onClick(user => {
+      user.prompt(text, true).then(res => {
+        if (res.submitted) {
+            // clicked 'OK'
+        }
+        else {
+            // clicked 'Cancel'
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    });
+    return button;
+  }
+
 }
